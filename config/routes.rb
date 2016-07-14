0916851a5d0803cfa48252e5b06b41dc86758a8f
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-14 16:52:18 +0200

    add review controller new, create and associates routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 
   root 'restaurants#index'
 
